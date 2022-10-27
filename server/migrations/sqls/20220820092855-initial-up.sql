904392f2b56a0b07f5b002391f6d04052a75e6a8
k.stoyanov <k.stoyanov@smartweb.com.ua>
2022-10-27 19:14:45 +0300

    finished initial DB migration file

--- server/migrations/sqls/20220820092855-initial-up.sql
+++ server/migrations/sqls/20220820092855-initial-up.sql
@@ -21,3 +21,19 @@ CREATE TABLE question_answer
     CONSTRAINT question_id_fk FOREIGN KEY (question_id) REFERENCES questions (id) ON DELETE CASCADE,
     CONSTRAINT answer_id_fk FOREIGN KEY (answer_id) REFERENCES answers(id) ON DELETE CASCADE
 );
+
+CREATE TABLE votes
+(
+    id INT NOT NULL,
+    question_id INT NOT NULL,
+    CONSTRAINT votes_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE votes_answer
+(
+    id INT NOT NULL,
+    vote_id INT NOT NULL,
+    answer_id INT NOT NULL,
+    CONSTRAINT vote_id_fk FOREIGN KEY (vote_id) REFERENCES votes (id) ON DELETE CASCADE,
+    CONSTRAINT answer_id_fk FOREIGN KEY (answer_id) REFERENCES answers(id) ON DELETE CASCADE
+);
